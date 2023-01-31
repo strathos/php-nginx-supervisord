441aaa7216d3d122b65acbdd0a5387126bd2aa32
Kari Honkasalo <kari.honkasalo@gmail.com>
2023-01-31 19:01:29 +0200

    feat: Add a bit more logging to start-up

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -14,19 +14,38 @@ if [ -n "${PGID-}" ]; then
 fi
 
 until nc -z -v -w30 "${DB_HOST}" 3306; do
-  echo "Waiting for database connection..."
+  echo
+  echo "STARTUP: Waiting for database connection..."
+  echo
   sleep 5
 done
 
 if [ -d vendor/drush ]; then
+  echo
+  echo "DRUSH: Rebuilding cache..."
+  echo
   drush cr -y
+
+  echo
+  echo "DRUSH: Applying any database updates..."
+  echo
   drush updb -y
 
   if [ -d config/sync ]; then
+    echo
+    echo "DRUSH: Importing config..."
+    echo
     drush cim -y
+
+    echo
+    echo "DRUSH: Rebuilding cache..."
+    echo
     drush cr -y
   fi
 
 fi
 
+echo
+echo "STARTUP: Starting supervisord..."
+echo
 exec "${@}"
