2e543fde4fccc81b50d5bbdc11f6c6e851987eff
Kari Honkasalo <kari.honkasalo@gmail.com>
2023-08-22 21:10:19 +0000

    feat: Fix editorconfig error

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -2,47 +2,46 @@
 set -e
 
 if [ -n "${PUID-}" ]; then
-  usermod -u "${PUID}" www-data
-  chown www-data:www-data /home/www-data
-  chown -R www-data:www-data /var/www
+    usermod -u "${PUID}" www-data
+    chown www-data:www-data /home/www-data
+    chown -R www-data:www-data /var/www
 fi
 
 if [ -n "${PGID-}" ]; then
-  groupmod -g "${PGID}" www-data
-  chown -R www-data:www-data /home/www-data
-  chown -R www-data:www-data /var/www
+    groupmod -g "${PGID}" www-data
+    chown -R www-data:www-data /home/www-data
+    chown -R www-data:www-data /var/www
 fi
 
 until nc -z -v -w30 "${DB_HOST}" 3306; do
-  echo
-  echo "STARTUP: Waiting for database connection..."
-  echo
-  sleep 5
+    echo
+    echo "STARTUP: Waiting for database connection..."
+    echo
+    sleep 5
 done
 
 if [ -d vendor/drush ]; then
-  echo
-  echo "DRUSH: Rebuilding cache..."
-  echo
-  drush cr -y
-
-  echo
-  echo "DRUSH: Applying any database updates..."
-  echo
-  drush updb -y
-
-  if [ -d config/sync ]; then
-    echo
-    echo "DRUSH: Importing config..."
-    echo
-    drush cim -y
-
     echo
     echo "DRUSH: Rebuilding cache..."
     echo
     drush cr -y
-  fi
 
+    echo
+    echo "DRUSH: Applying any database updates..."
+    echo
+    drush updb -y
+
+    if [ -d config/sync ]; then
+        echo
+        echo "DRUSH: Importing config..."
+        echo
+        drush cim -y
+
+        echo
+        echo "DRUSH: Rebuilding cache..."
+        echo
+        drush cr -y
+    fi
 fi
 
 echo
